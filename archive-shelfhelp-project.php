ffe284a90c620f278190e4867ede4985e744e845
Christopher Vasquez <chris.vqz@gmail.com>
2016-11-19 23:35:43 -0500

    Add projects archive and update navbar

--- archive-shelfhelp-project.php
+++ archive-shelfhelp-project.php
@@ -17,48 +17,73 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="full-width bg-white">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<?php include('template-parts/navBar.php'); ?>
+			</div>
+		</div>
+	</div>
+</div>
 
-		<?php if ( have_posts() ) : ?>
+<div class="container-fluid bg-gray03 xs-ptlg xs-pbxl">
+	<div class="row">
+		<div class="col-sm-12">
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+			<div class="container">
+
+				<?php if ( have_posts() ) : ?>
+
+					<div class="row">
+						<div class="col-sm-12">
+							<?php
+								the_archive_title( '<h1 class="xs-mtlg">', '</h1>' );
+							?>
+						</div>
+					</div>
+
+					<?php
+					// Start the Loop.
+					while ( have_posts() ) : the_post();
+					?>
+
+					<div class="case-study xs-mtxl">
+						<div class="case-study__container">
+							<img src="<?php the_post_thumbnail_url(); ?>" class="img-fluid" />
+
+							<div class="case-study__content">
+								<h2><?php echo get_the_title(); ?></h2>
+								<p>
+									<b><?php the_field('tagline'); ?></b>
+								</p>
+								<p>
+									<?php echo get_the_content(); ?>
+								</p>
+							</div>
+						</div>
+						<div style="clear:both;"></div>
+					</div>
+
+					<?php
+					// End the loop.
+					endwhile;
+
+					// Previous/next page navigation.
+					the_posts_pagination( array(
+						'prev_text'          => __( 'Previous page', 'twentysixteen' ),
+						'next_text'          => __( 'Next page', 'twentysixteen' ),
+						'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
+					) );
+
+				// If no content, include the "No posts found" template.
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+
+				endif;
 				?>
-			</header><!-- .page-header -->
 
-			<?php
-			// Start the Loop.
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			// End the loop.
-			endwhile;
-
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-				'next_text'          => __( 'Next page', 'twentysixteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-			) );
-
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+			</div>
+		</div>
+	</div>
+</div>
--- template-parts/navBar.php
+++ template-parts/navBar.php
@@ -1,19 +1,19 @@
 <div class="navbar">
   <div class="navbar__logo">
-    <img src="<?php bloginfo('template_directory');?>/images/logo.jpg" alt="Shelf Help logo" height="51" width="38" />
+    <a href="<?php echo get_site_url(); ?>"><img src="<?php bloginfo('template_directory');?>/images/logo.jpg" alt="Shelf Help logo" height="51" width="38" /></a>
   </div>
-  <div class="navbar__link">
+  <!-- <div class="navbar__link">
     <a href="<?php echo get_site_url(); ?>/about">Services</a>
-  </div>
+  </div> -->
   <div class="navbar__link">
-    <a href="<?php echo get_site_url(); ?>/services">Projects</a>
+    <a href="<?php echo get_site_url(); ?>/our-projects">Projects</a>
   </div>
-  <div class="navbar__link">
+  <!-- <div class="navbar__link">
     <a href="<?php echo get_site_url(); ?>/vlog">About</a>
   </div>
   <div class="navbar__link">
     <a href="<?php echo get_site_url(); ?>/reviews">Blog</a>
-  </div>
+  </div> -->
   <div class="navbar__spacer"></div>
   <div class="navbar__link navbar--teal">
     <a href="<?php echo get_site_url(); ?>/reviews">Contact</a>
